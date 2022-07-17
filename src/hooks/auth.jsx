c94a2eba289bd94e564cc6785dc9d809e0640d76
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2022-07-17 20:16:46 -0300

    feature

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState} from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 import { api } from '../services/api';
 
@@ -10,9 +10,12 @@ function AuthProvider({ children }){
 
   async function signIn({ email, password}) {
     try{
-      const response = await api.post("/sessions",[ email, password]);
+      const response = await api.post("/sessions",{ email, password});
       const { user, token } = response.data;
       
+      localStorage.setItem("@rocketnotes:user",JSON.stringify(user));
+      localStorage.setItem("@rocketnotes:token", token);
+
       api.defaults.headers.authorization = `Bearer ${token}`;
       setData({ user, token});
 
@@ -24,8 +27,22 @@ function AuthProvider({ children }){
       }
     }
   }
+
+  useEffect(() => {
+    const token = localStorage.getItem("@rocketnotes:token");
+    const user = localStorage.getItem("@rocketnotes:user");
+
+    if(token && user){
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
+      setData({
+        token,
+        user: JSON.parse(user)
+      })
+    }
+  },[]); 
   return(
-    <AuthContext.Provider value={{ signIn, user: data.user}}>
+    <AuthContext.Provider value={{ signIn, user: data.user }}>
       {children}
     </AuthContext.Provider>
 
