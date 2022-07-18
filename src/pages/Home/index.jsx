571c7b226a3586f7a8191feb8bfe578ea5d3ed78
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2022-07-18 14:56:06 -0300

    fix: tags

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,8 +5,24 @@ import { FiPlus, FiSearch } from 'react-icons/fi';
 import { Input } from "../../components/Input";
 import { Section } from "../../components/Section";
 import { Note } from "../../components/Note";
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
 
 export function Home() {
+
+  const [tags, setTags] = useState([]);
+
+
+
+  useEffect(() => {
+    async function fetchTags() {
+      const response = await api.get("/tags");
+      setTags(response.data);
+    }
+
+    fetchTags();
+  },[]);
+
   return(
     <Container>
       <Brand>
@@ -14,10 +30,21 @@ export function Home() {
       </Brand>
       <Header/>
       <Menu>
-        <li><ButtonText title="Todos" isActive/></li>
-        <li><ButtonText title="Nodejs"/></li>
-        <li><ButtonText title="React"/></li>
-
+        <li>
+          <ButtonText 
+            title="Todos"
+            isActive
+            />
+        </li>
+        {
+          tags && tags.map(tag => (
+            <li key={String(tag.id)}>
+              <ButtonText 
+                title={tag.name}
+              />
+            </li>
+          ))
+        }
       </Menu>
       <Search>
         <Input placeholder="pesquisar" icon={FiSearch}/>
